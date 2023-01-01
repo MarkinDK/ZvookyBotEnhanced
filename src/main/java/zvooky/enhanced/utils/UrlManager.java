d263dc3089f67f365f484839574587ecd90273c1
Anton M <anton.markin1008@yandex.ru>
2023-01-01 18:54:09 +0300

    Download of shorts added

--- src/main/java/zvooky/enhanced/utils/UrlManager.java
+++ src/main/java/zvooky/enhanced/utils/UrlManager.java
@@ -16,38 +16,36 @@ import java.util.regex.Pattern;
 @Setter
 @PropertySource("classpath:application.yml")
 public class UrlManager {
-    //    @Value("${standard}")
     private String STANDARD_URL_PATTERN;// = "(https://www\\.youtube\\.com/watch\\?v=)[\\w]{11}";
-    //    @Value("${m}")
     private String M_URL_PATTERN;// = "(https://m\\.youtube\\.com/watch\\?v=)[\\w]{11}";
-    //    @Value("${be}")
     private String BE_URL_PATTERN;// = "(https://youtu\\.be/)[\\w]{11}";
+    private String SHORTS_URL_PATTERN;// = "(https://youtube\\.com/shorts/)[\\w|\\-|_]{11}";
+    private String WSHORTS_URL_PATTERN;// = "(https://www\\.youtube\\.com/shorts/)[\\w|\\-|_]{11}";
 
     private final Pattern patternStandard;
     private final Pattern patternM;
     private final Pattern patternBe;
+    private final Pattern patternShorts;
+    private final Pattern patternWShorts;
 
     public UrlManager(
             @Value("${standard}") String STANDARD_URL_PATTERN,
             @Value("${m}") String M_URL_PATTERN,
-            @Value("${be}") String BE_URL_PATTERN
+            @Value("${be}") String BE_URL_PATTERN,
+            @Value("${shorts}") String SHORTS_URL_PATTERN,
+            @Value("${wshorts}") String WSHORTS_URL_PATTERN
     ) {
         patternStandard = Pattern.compile(STANDARD_URL_PATTERN);
         patternM = Pattern.compile(M_URL_PATTERN);
         patternBe = Pattern.compile(BE_URL_PATTERN);
+        patternShorts = Pattern.compile(SHORTS_URL_PATTERN);
+        patternWShorts = Pattern.compile(WSHORTS_URL_PATTERN);
         this.STANDARD_URL_PATTERN = STANDARD_URL_PATTERN;
         this.M_URL_PATTERN = M_URL_PATTERN;
         this.BE_URL_PATTERN = BE_URL_PATTERN;
-    }
+        this.SHORTS_URL_PATTERN = SHORTS_URL_PATTERN;
+        this.WSHORTS_URL_PATTERN = WSHORTS_URL_PATTERN;
 
-    public boolean isUrlCorrect(String url) {
-
-        Matcher matcherStandard = patternStandard.matcher(url);
-        Matcher matcherM = patternM.matcher(url);
-        Matcher matcherBe = patternBe.matcher(url);
-        return url.contains("https://www.youtube.com/watch")
-                || url.contains("https://m.youtube.com/watch")
-                || url.contains("https://youtu.be/");
     }
 
     public String refineUrl(String text) {
@@ -55,6 +53,8 @@ public class UrlManager {
         Matcher matcherStandard = patternStandard.matcher(text);
         Matcher matcherM = patternM.matcher(text);
         Matcher matcherBe = patternBe.matcher(text);
+        Matcher matcherShorts = patternShorts.matcher(text);
+        Matcher matcherWShorts = patternWShorts.matcher(text);
         String result = "";
         if (matcherStandard.find()) {
             result = matcherStandard.group();
@@ -62,6 +62,10 @@ public class UrlManager {
             result = matcherM.group();
         } else if (matcherBe.find()) {
             result = matcherBe.group();
+        }else if (matcherShorts.find()) {
+            result = matcherShorts.group();
+        }else if (matcherWShorts.find()) {
+            result = matcherWShorts.group();
         }
         return result;
     }
